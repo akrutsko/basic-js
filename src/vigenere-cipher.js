a0dde4b01a0d03d2c76173b74c06f628441c9aef
Aliaksei Krutsko <wonder4ik@gmail.com>
2023-04-17 00:27:45 +0300

    feat: implement vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,79 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(reverse) {
+    this.reverse = reverse;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(text, key) {
+    if (text === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!')
+    }
+
+    text = text.toUpperCase();
+
+    if (this.reverse === false) {
+      text = text.split('').reverse().join('');
+    }
+
+    key = key.toUpperCase().repeat(Math.ceil(text.length / key.length))
+
+    const codeA = 'A'.charCodeAt(0);
+    const codeZ = 'Z'.charCodeAt(0);
+    const alphabet = 26;
+
+    const encrypted = [];
+
+    for (let k = 0, i = 0; i < text.length; i++) {
+      const letter = text[i];
+      const codeLetter = letter.charCodeAt(0);
+
+      if (codeZ < codeLetter || codeLetter < codeA) {
+        encrypted.push(letter)
+      } else {
+        const letterIdx = codeLetter - codeA;
+        const shift = key.charCodeAt(k) - codeA;
+        encrypted.push(String.fromCharCode(codeA + (letterIdx + shift) % alphabet));
+        k++;
+      };
+    }
+
+    return encrypted.join('');
+  }
+
+  decrypt(text, key) {
+    if (text === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!')
+    }
+
+    if (this.reverse === false) {
+      text = text.split('').reverse().join('');
+    }
+
+    key = key.toUpperCase().repeat(Math.ceil(text.length / key.length))
+
+    const codeA = 'A'.charCodeAt(0);
+    const codeZ = 'Z'.charCodeAt(0);
+    const alphabet = 26;
+
+    const decrypted = [];
+
+    for (let k = 0, i = 0; i < text.length; i++) {
+      const letter = text[i];
+      const codeLetter = letter.charCodeAt(0);
+
+      if (codeZ < codeLetter || codeLetter < codeA) {
+        decrypted.push(letter)
+      } else {
+        const letterIdx = codeLetter - codeA;
+        const shift = key.charCodeAt(k) - codeA;
+        decrypted.push(String.fromCharCode(codeA + (letterIdx - shift + alphabet) % alphabet));
+        k++;
+      };
+    }
+
+    return decrypted.join('');
   }
 }
 
