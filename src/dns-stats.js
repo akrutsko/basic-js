11824fccd01ef326feaffb8a954a4af76324b8b6
Aliaksei Krutsko <wonder4ik@gmail.com>
2023-04-16 11:06:20 +0300

    feat: implement dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  return domains.reduce((acc, x) => {
+    const subdomains = x.split('.').reverse();
+
+    let temp = '';
+    for (const subdomain of subdomains) {
+      temp += '.' + subdomain;
+      temp in acc ? acc[temp]++ : acc[temp] = 1;
+    }
+
+    return acc;
+  }, {})
 }
 
 module.exports = {
